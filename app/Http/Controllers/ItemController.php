293a0687f2abf616bb94140c756ddc78daafa231
rommel dalisay <rommelmdalisay@gmail.com>
2025-02-11 09:06:11 +0800

    create item form

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -19,7 +19,7 @@ class ItemController extends Controller
      */
     public function create()
     {
-        //
+        return view('item.create');
     }
 
     /**
@@ -27,7 +27,7 @@ class ItemController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        dd($request);
     }
 
     /**
--- resources/views/item/create.blade.php
+++ resources/views/item/create.blade.php
@@ -2,5 +2,16 @@
 
 @section('body')
     <div class="container">
+        {!! Form::open(['route' => 'items.store']) !!}
+        {!! Form::label('desc', 'item name', ['class' => 'form-label']) !!}
+        {!! Form::text('description', null, ['class' => 'form-control', 'id' => 'desc']) !!}
+        
+        {!! Form::label('cost', 'cost price', ['class' => 'form-label']) !!}
+        {!! Form::number('cost_price', 0.00, ['min' => 0.00, 'step' => 0.01, 'class' => 'form-control', 'id' => 'cost' ]) !!}
+
+        {!! Form::label('sell', 'sell price', ['class' => 'form-label']) !!}
+        {!! Form::number('sell_price', 0.00, ['min' => 0.00, 'step' => 0.01, 'class' => 'form-control', 'id' => 'sell' ]) !!}
+        {!! Form::submit('Add item',  ['class'=> "btn btn-primary"]) !!}
+        {!! Form::close() !!}
     </div>
 @endsection
